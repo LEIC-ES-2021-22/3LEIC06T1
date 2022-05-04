7862564567fd3793d67d544bd9e0d63c3b9132f8
luis-viegas <luiscarlosviegas@icloud.com>
2022-05-05 00:46:11 +0100

    all done

--- src/lib/view/Pages/map_page_view.dart
+++ src/lib/view/Pages/map_page_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:uni/view/Pages/general_page_view.dart';
 import 'package:uni/view/Widgets/terms_and_conditions.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPageView extends StatefulWidget {
   @override
@@ -10,28 +11,35 @@ class MapPageView extends StatefulWidget {
 
 /// Manages the 'about' section of the app.
 class AboutPageViewState extends GeneralPageViewState {
+
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(41.17817607238003, -8.596451894896566);
+
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+
   @override
   Widget getBody(BuildContext context) {
     final MediaQueryData queryData = MediaQuery.of(context);
-    return ListView(
-      children: <Widget>[
-        Text(
-          'Mapa',
-          textAlign: TextAlign.center,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text("Feup's Map")),
+          backgroundColor: Colors.cyan,
         ),
-        Center(
-            child: Padding(
-              padding: EdgeInsets.only(
-                  left: queryData.size.width / 12,
-                  right: queryData.size.width / 12,
-                  top: queryData.size.width / 12,
-                  bottom: queryData.size.width / 12),
-              child: Text(
-                'Ola eu sou um widget',
-                textAlign: TextAlign.center,
-                )
-            ))
-      ],
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 17.0,
+            bearing: 90.0,
+          ),
+        ),
+      ),
     );
   }
 }
