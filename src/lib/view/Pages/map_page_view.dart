85d99626bea02bf8c9b5e2762286e3c26c8c7577
luis-viegas <luiscarlosviegas@icloud.com>
2022-05-17 17:20:03 +0100

    location marker added

--- src/lib/view/Pages/map_page_view.dart
+++ src/lib/view/Pages/map_page_view.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:uni/view/Pages/general_page_view.dart';
 import 'package:uni/view/Widgets/terms_and_conditions.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapPageView extends StatefulWidget {
   @override
@@ -11,15 +14,6 @@ class MapPageView extends StatefulWidget {
 
 /// Manages the 'about' section of the app.
 class AboutPageViewState extends GeneralPageViewState {
-
-  GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(41.17817607238003, -8.596451894896566);
-
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
 
 
   @override
@@ -31,15 +25,86 @@ class AboutPageViewState extends GeneralPageViewState {
           title: const Center(child: Text("Mapa")),
           backgroundColor: Colors.cyan,
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 17.0,
-            bearing: 90.0,
-          ),
-        ),
+        body: MapSample(),
       ),
     );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> _markers = {};
+  BitmapDescriptor currentPosIcon;
+
+  void setIcon() async {
+    currentPosIcon = await BitmapDescriptor.fromAssetImage(ImageConfiguration(), 'assets/images/myPos.png');
+    //currentPosIcon = BitmapDescriptor.defaultMarker;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setIcon();
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId("_kGooglePlex"),
+          infoWindow: InfoWindow(title: "Estás aqui"),
+          icon: currentPosIcon,
+          position: LatLng(41.17817607238003, -8.596451894896566),
+        ),
+      );
+    });
+  }
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(41.17817607238003, -8.596451894896566),
+    zoom: 17.0,
+    bearing: 90.0,
+  );
+
+  static final Marker _kGooglePlexMarker = Marker(
+    markerId: MarkerId("_kGooglePlex"),
+    infoWindow: InfoWindow(title: "Test Window"),
+    icon: BitmapDescriptor.defaultMarker,
+    position: LatLng(41.17817607238003, -8.596451894896566),
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        markers: _markers,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: _onMapCreated,
+      ),
+      /*floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),*/
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
