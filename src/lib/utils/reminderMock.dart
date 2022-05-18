b13def608c2f5110b50d1a6ed089ee0f845dbe87
Joao Andrade <up201905589@fe.up.pt>
2022-05-18 10:02:03 +0100

    Added widget to change time

--- src/lib/utils/reminderMock.dart
+++ src/lib/utils/reminderMock.dart
@@ -5,7 +5,6 @@ class ReminderMock{
     return [
       Reminder(1, 201905589, 'SDInf', 'Não esquecer de ir tratar das justificações de falta',
           '2022-05-28 14:00'),
-
       Reminder(2, 201905589, 'STMA', 'Cacifos', '2022-05-30 15:00')
     ];
   }
