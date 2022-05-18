92f2c185623b5d73390255e124b0026d0e4ca472
Sergio Estevao <smestevao11@gmail.com>
2022-05-18 07:40:55 +0100

    refactor: divided tasks in constructor by funcs

--- src/lib/controller/reminder_controller.dart
+++ src/lib/controller/reminder_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'dart:async';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 import 'package:flutter_native_timezone/flutter_native_timezone.dart';
@@ -12,38 +13,71 @@ class NotificationService {
 
 
   NotificationService(){
-    final androidInit = AndroidInitializationSettings('icon');
-    final iOSInit = IOSInitializationSettings();
-    final initSettings = InitializationSettings(android: androidInit,
-                                              iOS: iOSInit);
-    reminderNotifications.initialize(initSettings,
-        onSelectNotification: onSelectNotification);
-
-    final androidDetails = AndroidNotificationDetails('Reminder ID',
-                                                      'Reminders');
-    final iOSDetails = IOSNotificationDetails();
-    generalNotificationDetails = NotificationDetails(android: androidDetails,
-                                                     iOS: iOSDetails);
-    _setupTimezone();
+    _setupNotifications();
     idCounter = 0;
   }
 
-  Future _setupTimezone() async{
+  _setupNotifications() async{
+    await _setupTimezone();
+    await _initializeNotifications();
+  }
+
+  Future<void> _setupTimezone() async{
     tz.initializeTimeZones();
     final String currentTimeZone = await FlutterNativeTimezone.getLocalTimezone();
     tz.setLocalLocation(tz.getLocation(currentTimeZone));
   }
 
-  Future onSelectNotification(String payload) async{
-    //do nothing
+  _initializeNotifications (){
+    final androidInit = AndroidInitializationSettings('icon');
+    final iOSInit = IOSInitializationSettings();
+    final initSettings = InitializationSettings(
+        android: androidInit,
+        iOS: iOSInit
+    );
+    reminderNotifications.initialize(
+        initSettings,
+        onSelectNotification: _onSelectNotification
+    );
+
+    final androidDetails = AndroidNotificationDetails(
+      'Reminder ID',
+      'Reminders',
+      channelDescription: "Reminder notification",
+      importance: Importance.max,
+      priority: Priority.max,
+      enableVibration: true,
+    );
+    final iOSDetails = IOSNotificationDetails();
+    generalNotificationDetails = NotificationDetails(android: androidDetails,
+        iOS: iOSDetails);
+  }
+
+
+
+  Future _onSelectNotification(String payload) async{
+    //if(payload != null && payload.isNotEmpty){
+    //
+    //}
+  }
+
+  checkForNotifications() async{
+    final details = await reminderNotifications.getNotificationAppLaunchDetails();
+    if (details != null && details.didNotificationLaunchApp){
+      _onSelectNotification(details.payload);
+    }
   }
 
   Future addNotification(DateTime notifSchedule, Service service) async{
 
-    await reminderNotifications.zonedSchedule(idCounter, "Nome-Serviço",
-        "dia e hora", tz.TZDateTime.from(notifSchedule, tz.local), generalNotificationDetails,
+    await reminderNotifications.zonedSchedule(idCounter,
+        'Nome-Serviço',
+        'dia e hora',
+        tz.TZDateTime.from(notifSchedule, tz.local), generalNotificationDetails,
         androidAllowWhileIdle: true,
-        uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime);
+        uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime
+    );
+
     idCounter += 1;
   }
 
